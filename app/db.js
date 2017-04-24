bd08d599ad11f7888f8570dbce6f532671ac3109
Teerapath Sujitto <teerapart.s@gmail.com>
2017-04-25 04:35:54 +0700

    Implementing failover mitigation

--- app/db.js
+++ app/db.js
@@ -1,6 +1,15 @@
 const redis = require('redis');
+const bluebird = require('bluebird');
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
 var loredis = redis.createClient();
 var reredis = redis.createClient();
-exports.createUser = function(){
+function get(){
+
+}
+function getUser(username){
+    return get(`users:${username}`);
+}
+exports.createUser = function(username,password){
     
 }
--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+var http = require('http');
 global.cfg = require('config');
+global.failoverState = 'unknown';
+global.getRemoteFailoverState = function(){
+    return new Promise(function(resolve,reject){
+
+    });
+}
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
-
 if(cluster.isMaster){
+
     for(let i = 0;i<numCPUs;++i)
         cluster.fork();
-        cluster.on('exit',(worker,code,signal)=>{
-           console. log(`worker ${worker.pid} died`);
-        });
+    cluster.on('exit',(worker,code,signal)=>{
+        console. log(`worker ${worker.pid} died`);
+    });
 } 
 else{
     require('./app/main.js');
